3ee04dae1b287896120aa9e7cbd3363fbbd0c65b
Hakimeh Mordadi <hakimehmordadi@gmail.com>
2020-08-20 01:36:51 +0430

    Update and rename Clean Code to CleanCode.sh

---
+++ CleanCode.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+##############################################################################
+# SHORTCUTS and HISTORY
+##############################################################################
+
+#Clean Code
+
+###We need to make sure thet we're creating code that can be easily understood by humans.
+
+
+##What Is Clean Code?  
+
+       ######<> Written with human in mind
+       ######<> Names are easy to understand
+       ######<> Formatted consistently
+       ######<> Clearly communication in tent
+       
+That's where clean code principles come into play. By following them, you will ensure that youre crafting code that will be understood.
